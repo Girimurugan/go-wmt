182a2115382155f656942e75b6d3ca2c0a3174a7
girimurugan <girimurugan.natarajan@gmail.com>
2020-08-01 19:34:21 +0530

    commits

---
+++ args.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	var s int64
+
+    argsWithProg := os.Args
+    argsWithoutProg := os.Args[1:]
+
+    arg := os.Args[3]
+
+    fmt.Println(argsWithProg)
+    fmt.Println(argsWithoutProg)
+	fmt.Println(arg)
+	
+	for _, arg := range os.Args[1:]{
+
+		n, _ := strconv.ParseInt(arg, 10, 10)
+		s = s + n
+	}
+	fmt.Printf("Total is %d", s)
+}
--- kafka/kafka.go
+++ kafka/kafka.go
@@ -6,3 +6,23 @@ import (
 	"github.com/segmentio/kafka-go/snappy"
 	"github.com/segmentio/kafka-go"
 )
+
+func main(){
+
+// to produce messages
+topic := "first_topic"
+partition := 0
+
+conn, _ := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+
+conn.SetWriteDeadline(time.Now().Add(10*time.Second))
+conn.WriteMessages(
+    kafka.Message{Value: string("one!")},
+    kafka.Message{Value: []byte("two!")},
+    kafka.Message{Value: []byte("three!")},
+)
+
+conn.Close()
+
+
+}
